332d40f2b0113dbb2adcedcfaac2b42367bacd9b
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-13 10:00:14 -0400

    completed exercise 3

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,11 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        System.out.println("Addition: 10 + 10 = " + (10 + 10));
+        System.out.println("Subtraction: 10 - 10 = " + (10 - 10));
+        System.out.println("Multiplication: 10 * 10 = " + (10 * 10));
+        System.out.println("Division: 10 / 10 = " + (10 / 10));
+        System.out.println("Modulus: 10 % 10 = " + (10 % 10));
 
     }
 
