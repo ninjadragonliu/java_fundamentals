aeb9482f27bd9e2b782c7f46c87f3e925b671b62
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-13 10:13:07 -0400

    completed exercise 7

--- src/labs_examples/datatypes_operators/labs/Exercise_07.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_07.java
@@ -24,6 +24,8 @@ public class Exercise_07 {
         int days = scanner.nextInt();
 
         // write completed code here
+        int seconds = days * 60 * 60 * 24;
+        System.out.println(seconds);
 
     }
 }
