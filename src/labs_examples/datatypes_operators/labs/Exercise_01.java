adaaeb59709fe7c4848c7b57563a0049a4fea361
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-13 09:50:16 -0400

    completed exercise 1

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -17,6 +17,14 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        int i = 10;
+        double d = 10.5;
+        float f = 10.5f;
+        char c = 'A';
+        System.out.println("int: " + i);
+        System.out.println("double: " + d);
+        System.out.println("float: " + f);
+        System.out.println("char: " + c);
 
     }
 }
