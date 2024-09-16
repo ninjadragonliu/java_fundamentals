ceaa073144503c1101b4d28d00f71d29afce4d11
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-16 16:17:47 -0400

    completed exercise 1

---
+++ src/labs_examples/objects_classes_methods/labs/objects/Airplane.java
@@ -0,0 +1,23 @@
+package labs_examples.objects_classes_methods.labs.objects;
+
+public class Airplane {
+
+    AirplaneEngine engine;
+    Wing wing;
+    Cockpit cockpitl;
+    LandingGear landingGear;
+    double fuelCapacity;
+    double fuelLevel;
+}
+class Wing {
+    double wingSpan;
+}
+class AirplaneEngine {
+    double horsePower;
+}
+class Cockpit {
+    int cockpitHeight;
+}
+class LandingGear {
+    int landingGearHeight;
+}
