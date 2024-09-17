14ca99f0b4450cb3ccd4afbbb3fdd00558fb3c02
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-17 12:54:09 -0400

    created parameterized constructor

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Animal/Animal.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Animal/Animal.java
@@ -1,9 +1,25 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance.Animal;
 
-abstract class Animal
+class Animal
 {
     private String name;
     private String sound;
+
+    public Animal(String name, String sound)
+    {
+        this.name = name;
+        this.sound = sound;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSound()
+    {
+        return sound;
+    }
 }
 
 
