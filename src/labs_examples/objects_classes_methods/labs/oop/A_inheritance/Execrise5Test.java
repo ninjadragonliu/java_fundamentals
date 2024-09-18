0a036d8380a4e31db76b3490934ef340fe38904c
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-18 17:39:10 -0400

    completed exercise 5

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Execrise5Test.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Execrise5Test.java
@@ -1,4 +1,24 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
 public class Execrise5Test {
+    public static void main(String[] args)
+    {
+        Parent1 c = new Child1();
+    }
+}
+
+class Parent1
+{
+    public Parent1()
+    {
+        System.err.println("Parent called");
+    }
+}
+
+class Child1 extends Parent1
+{
+    public Child1()
+    {
+        System.err.println("Child called");
+    }
 }
