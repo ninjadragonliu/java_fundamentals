420100d966792c5b1855fa393046d8cb4378019a
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-18 17:14:18 -0400

    completed exercise 4

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise4Test.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise4Test.java
@@ -1,4 +1,25 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
 public class Exercise4Test {
+    public static void main(String[] args)
+    {
+        Parent c = new Child();
+        c.doSomething();
+    }
+}
+
+class Parent
+{
+    public void doSomething()
+    {
+        System.err.println("Parent called");
+    }
+}
+
+class Child extends Parent
+{
+    public void doSomething()
+    {
+        System.err.println("Child called");
+    }
 }
