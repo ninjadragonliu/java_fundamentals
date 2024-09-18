8c9ac28be79fbe7308fb4126c81e5d80f64b49ed
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-18 12:32:43 -0400

    testing inheritance

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Animal/AnimalController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Animal/AnimalController.java
@@ -1,4 +1,15 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance.Animal;
 
 public class AnimalController {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", "Sound");
+        animal.setSpeed(10);
+        System.out.println(animal.move());
+        Bird bird = new Bird("Bird", "Sound");
+        bird.setSpeed(20);
+        System.out.println(bird.move());
+        bird.fly();
+        System.out.println(bird.noise());
+    }
 }
