e6d8f1eea66e40fb28db6eecd1ad0e3302b022ce
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-20 10:08:01 -0400

    tested overloading concept again

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Subtraction.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Subtraction.java
@@ -1,4 +1,26 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism;
 
 public class Subtraction {
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int subtract(int a, int b, int c) {
+        return a - b - c;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+}
+
+class SubtractionDemo {
+    public static void main(String args[]) {
+        Subtraction obj = new Subtraction();
+        int a = 15, b = 10, c = 5;
+        double d = 15.5, e = 10.5;
+        System.out.println("Subtraction of " + a + " and " + b + " is " + obj.subtract(a, b));
+        System.out.println("Subtraction of " + a + " and " + b + " and " + c + " is " + obj.subtract(a, b, c));
+        System.out.println("Subtraction of " + d + " and " + e + " is " + obj.subtract(d, e));
+    }
 }
