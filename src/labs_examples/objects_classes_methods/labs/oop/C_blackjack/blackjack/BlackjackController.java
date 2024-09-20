170395f638208cf46789eaaf8170bedd91896039
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-20 15:19:27 -0400

    revised blackjack code

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/blackjack/BlackjackController.java
@@ -1,5 +1,7 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack.blackjack;
 
+import java.util.Scanner;
+
 public class BlackjackController {
 
     public static void main(String[] args) {
@@ -10,11 +12,27 @@ public class BlackjackController {
         Deck deck = new Deck();
         Player player = new Player();
         Player computer = new Player();
-
+        Scanner sc = new Scanner(System.in);
         deck.deal(player);
         deck.deal(player);
         deck.deal(computer);
         deck.deal(computer);
-        System.out.println("Your hand is " + player.hand);
+        System.out.println("Your hand is " + player.hand.getHandValue());
+        System.out.println("Do you want another card?");
+        String answer = sc.nextLine();
+        while (answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes")) {
+            deck.deal(player);
+            System.out.println("Your hand is " + player.hand.getHandValue());
+            if (player.hand.isBusted()) {
+                System.out.println("You busted!");
+                break;
+            }
+            System.out.println("Do you want another card?");
+            answer = sc.nextLine();
+        }
+        System.out.println("Your hand is " + player.hand.getHandValue());
+        if (player.hand.isBusted()) {
+            System.out.println("You busted!");
+        }
     }
 }
