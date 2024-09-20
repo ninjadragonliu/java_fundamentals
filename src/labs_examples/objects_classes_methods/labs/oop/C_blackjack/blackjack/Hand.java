6b251f340f2a33cc43426062d754d94f0a804f59
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-20 15:47:22 -0400

    spruced up blackjack code

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/blackjack/Hand.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/blackjack/Hand.java
@@ -19,7 +19,7 @@ public class Hand {
     }
 
     public int getHandValue() {
-        return handValue;
+        return calculateHandValue();
     }
 
     public void setHandValue(int handValue) {
@@ -27,9 +27,34 @@ public class Hand {
     }
     public void addCard(Card card){
         cards.add(card);
-        handValue += card.getCardValue();
     }
     public boolean isBusted(){
         return handValue > 21;
+    }
+    // Adjust hand value based on Ace being 1 or 11
+    private int calculateHandValue() {
+        int totalValue = 0;
+        int numAces = 0;
+
+        // First pass: calculate value, treating Aces as 1 for now
+        for (Card card : cards) {
+            int cardValue = card.getCardValue();
+            if (cardValue == 1) {
+                numAces++;  // Track the number of Aces
+                totalValue += 1;  // Treat Ace as 1 for now
+            } else if (cardValue > 10) {
+                totalValue += 10;  // Face cards (J, Q, K) are worth 10
+            } else {
+                totalValue += cardValue;
+            }
+        }
+
+        // Adjust Aces to 11 if it won't cause a bust
+        while (numAces > 0 && totalValue + 10 <= 21) {
+            totalValue += 10;  // Upgrade an Ace from 1 to 11
+            numAces--;
+        }
+
+        return totalValue;
     }
 }
