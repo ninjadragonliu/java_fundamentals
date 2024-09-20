2e0f62b237118a3b6ea21874c4ad9975acbe9e1a
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-20 10:00:46 -0400

    tested interface concept

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/InterfaceDemo.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/InterfaceDemo.java
@@ -1,4 +1,29 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism;
 
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
+
 public class InterfaceDemo {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape rectangle = new Rectangle();
+
+        circle.draw(); // Outputs: Drawing a Circle
+        rectangle.draw(); // Outputs: Drawing a Rectangle
+    }
 }
