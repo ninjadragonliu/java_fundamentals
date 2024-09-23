e86c42909d1a1edaaf644536ed356d92e3f38fb5
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-23 17:38:16 -0400

    working Digimon Battle Sim

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
@@ -1,4 +1,7 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_sim;
 
-public class Greymon {
+public class Greymon extends Agumon {
+    public Greymon(String name, int hp, int attack, int defense, int speed) {
+        super(name, hp, attack, defense, speed);
+    }
 }
