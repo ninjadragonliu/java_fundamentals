0c4afcb7084660f0c6cea2e73a4fc6a4e86ebe5a
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-23 12:32:33 -0400

    completed exercise 1 part 3 and 4

--- src/labs_examples/static_nonstatic/labs/Test.java
+++ src/labs_examples/static_nonstatic/labs/Test.java
@@ -4,15 +4,19 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("I'm in the main method");
+        System.out.println("I'm in the main method in Test");
         Test someObject = new Test();
         someObject.someMethod();
         someObject.someMethod2();
+
+        Test2 someObject2 = new Test2();
+        someObject2.someMethod();
+        someObject2.someMethod2();
     }
     public static void someMethod() {
-        System.out.println("I'm in a static method");
+        System.out.println("I'm in a static method in Test");
     }
     public void someMethod2() {
-        System.out.println("I'm in a non-static method");
+        System.out.println("I'm in a non-static method in Test");
     }
 }
---
+++ src/labs_examples/static_nonstatic/labs/Test2.java
@@ -0,0 +1,12 @@
+package labs_examples.static_nonstatic.labs;
+
+public class Test2 {
+
+    public static void someMethod() {
+        System.out.println("I'm in a static method in Test2");
+    }
+
+    public void someMethod2() {
+        System.out.println("I'm in a non-static method in Test2");
+    }
+}
