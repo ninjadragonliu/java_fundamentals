6ccb97de4333ecb7adc3eae33c8d385f326fbb7b
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-24 17:35:35 -0400

    completed exercise 4

--- src/labs_examples/exception_handling/labs/Exercise_04.java
+++ src/labs_examples/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,19 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+public class Exercise_04 {
+
+    public static void main(String[] args) {
+        try {
+            try {
+                System.out.println("try 1");
+                throw new Exception();
+            } catch (Exception e) {
+                System.out.println("catch 1");
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println("catch 2");
+        }
+    }
+}
