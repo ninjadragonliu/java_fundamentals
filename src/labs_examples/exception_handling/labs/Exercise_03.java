d5acbd7c950a1d1a0014a8974caffa105ea63e1e
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-24 17:34:20 -0400

    completed exercise 3

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,17 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+public class Exercise_03 {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("try");
+            throw new RuntimeException();
+        } catch (Exception e) {
+            System.out.println("catch");
+        } finally {
+            System.out.println("finally");
+        }
+    }
+}
