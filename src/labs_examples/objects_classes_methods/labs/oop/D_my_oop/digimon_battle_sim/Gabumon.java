8663b44b2f89cc2edd7c9267444945459bef1564
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-24 13:05:20 -0400

    modify attack method

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Gabumon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Gabumon.java
@@ -8,6 +8,7 @@ public class Gabumon extends Digimon {
 
     @Override
     public void attack(Digimon target, Attack chosenAttack) {
-
+        System.out.println(this.name + " uses " + chosenAttack.getName() + "!");
+        target.defend(chosenAttack.getDamage());
     }
 }
