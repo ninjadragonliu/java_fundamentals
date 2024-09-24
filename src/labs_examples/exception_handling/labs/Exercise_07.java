19acad10d3f7f842dc9028bdb64593dac794487b
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-24 17:43:35 -0400

    completed exercise 7

--- src/labs_examples/exception_handling/labs/Exercise_07.java
+++ src/labs_examples/exception_handling/labs/Exercise_07.java
@@ -6,3 +6,18 @@ package labs_examples.exception_handling.labs;
  *      1) Create a custom exception.
  *      2) Demonstrate a method throwing your custom exception.
  */
+public class Exercise_07 {
+
+    public static void main(String[] args) throws MyException {
+        int a = 10;
+        if(a == 10) {
+            throw new MyException("a is 10");
+        }
+    }
+}
+
+class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+}
