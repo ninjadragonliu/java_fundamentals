2c1ac227a74213986f8dafa9ef3c811956a9605b
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-24 17:36:38 -0400

    completed exercise 5

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -13,6 +13,11 @@ class Example {
 
     public static void main(String[] args) {
 
+        int test = 10;
+        int test2 = 0;
+        if(test2 == 0) {
+            throw new ArithmeticException();
+        }
     }
 
 }
