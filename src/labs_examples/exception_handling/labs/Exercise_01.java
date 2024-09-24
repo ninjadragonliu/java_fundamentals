efbda7998ce6821a9c31957b6cc45a017f6e2bc2
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-24 14:22:04 -0400

    completed exercise 1

--- src/labs_examples/exception_handling/labs/Exercise_01.java
+++ src/labs_examples/exception_handling/labs/Exercise_01.java
@@ -7,3 +7,16 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("0");
+            System.out.println(1 / 0);
+            System.out.println("2");
+        } catch (Exception e) {
+            System.out.println("3");
+        }
+    }
+}
