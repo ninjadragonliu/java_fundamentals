50c41e3c17f590413957b04895da30e8798d675f
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-24 09:39:11 -0400

    completed exercise 1

--- src/labs_examples/packages/labs/subpackage/Test.java
+++ src/labs_examples/packages/labs/subpackage/Test.java
@@ -1,4 +1,12 @@
 package labs_examples.packages.labs.subpackage;
 
 public class Test {
+    protected static void someMethod() {
+        System.out.println("I'm in a protected static method in Test");
+    }
+    public void someMethod2() {
+        System.out.println("I'm in a public non-static method in Test");
+    }
+
+
 }
--- src/labs_examples/packages/labs/subpackage2/Test2.java
+++ src/labs_examples/packages/labs/subpackage2/Test2.java
@@ -1,4 +1,17 @@
 package labs_examples.packages.labs.subpackage2;
 
+import labs_examples.packages.labs.subpackage.Test;
+
 public class Test2 {
+    public void someMethod() {
+        System.out.println("I'm in a non-static method in Test2");
+    }
+    public static void someMethod2() {
+        System.out.println("I'm in a static method in Test2");
+    }
+    public static void main(String[] args) {
+        Test someObject = new Test();
+        someObject.someMethod();
+        someObject.someMethod2();
+    }
 }
