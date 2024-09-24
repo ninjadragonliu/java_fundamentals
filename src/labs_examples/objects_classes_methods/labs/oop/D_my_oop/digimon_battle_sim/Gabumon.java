7d47a5169e68e878db7b7710c57bc86397bb593a
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-23 20:29:32 -0400

    working Digimon Battle Sim

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Gabumon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Gabumon.java
@@ -1,4 +1,8 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_sim;
 
-public class Gabumon {
+public class Gabumon extends Digimon {
+    public Gabumon(String name, int hp, int attack, int defense, int speed) {
+        super(name, hp, attack, defense, speed);
+    }
+
 }
