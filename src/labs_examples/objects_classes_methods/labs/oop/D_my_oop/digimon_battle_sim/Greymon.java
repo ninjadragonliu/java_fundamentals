fcf159fbab1caf6e453407aeb0fcee2de411cf99
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-25 19:54:02 -0400

    Adjusted Greymon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
@@ -2,7 +2,7 @@ package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_s
 
 public class Greymon extends Digimon{
     public Greymon() {
-        super("Greymon", "Vaccine", 150, 40, 25, 25;
+        super("Greymon", "Vaccine", 150, 40, 25, 25);
         this.addAttack(new Attack("Nova Blast", 50));
         this.addAttack(new Attack("Great Horn Attack", 60));
     }
