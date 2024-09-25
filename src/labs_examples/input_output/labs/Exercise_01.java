210e3588d86a706d1e2af39e6873b30eee710297
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-25 09:04:02 -0400

    completed exercise 1

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,11 @@
 package labs_examples.input_output.labs;
 
+import labs_examples.input_output.examples.BufferedByteStream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -10,9 +16,23 @@ package labs_examples.input_output.labs;
  */
 
 class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("src/labs_examples/input_output/files/char_data.txt");
+        BufferedInputStream inputStream = new BufferedInputStream(fileInputStream);
+        byte[] buffer = new byte[5];
+        int bytesRead = 0;
+        try {
 
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                // print out bytes read to console
+                System.out.print(new String(buffer, 0, bytesRead));
+            }
 
+            inputStream.close();
+            fileInputStream.close();
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
 
     }
 }
