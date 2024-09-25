7fd28e6c50e00d0477534a3aa8180da99d633147
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-25 19:53:38 -0400

    Adjusted Greymon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
@@ -1,7 +1,14 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_sim;
 
-public class Greymon extends Agumon {
+public class Greymon extends Digimon{
     public Greymon() {
-        super();
+        super("Greymon", "Vaccine", 150, 40, 25, 25;
+        this.addAttack(new Attack("Nova Blast", 50));
+        this.addAttack(new Attack("Great Horn Attack", 60));
+    }
+
+    @Override
+    public void attack(Digimon target, Attack chosenAttack) {
+
     }
 }
