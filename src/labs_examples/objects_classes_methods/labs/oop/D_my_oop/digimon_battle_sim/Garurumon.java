b4066a9646ea8aed3a31f7dd91289b594c6a2b54
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-25 19:56:21 -0400

    Adjusted Garurumon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Garurumon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Garurumon.java
@@ -1,8 +1,15 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_sim;
 
-public class Garurumon extends Gabumon{
+public class Garurumon extends Digimon{
 
     public Garurumon() {
-        super();
+        super("Garurumon", "Data", 125, 45, 45, 30);
+        this.addAttack(new Attack("Howling Blaster", 40));
+        this.addAttack(new Attack("Freeze Fang", 35));
+    }
+
+    @Override
+    public void attack(Digimon target, Attack chosenAttack) {
+
     }
 }
