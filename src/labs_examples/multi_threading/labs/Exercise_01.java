ce4a139e839788c5cd5943f5ef1bd0808ede653e
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-26 11:50:58 -0400

    started exercise 1

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -7,5 +7,31 @@ package labs_examples.multi_threading.labs;
  *      2: Demonstrate at least two distinct ways of initiating a Thread using the Runnable you just created
  *
  */
+public class Exercise_01 {
 
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        Thread t = new Thread(myThread, "MyThread");
+        t.start();
+    }
+}
+
+class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " starting");
+        try {
+            for(int count=0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + Thread.currentThread().getName() +
+                        ", count is " + count);
+            }
+        }
+        catch(InterruptedException exc) {
+            System.out.println(Thread.currentThread().getName() + " interrupted");
+        }
+        System.out.println(Thread.currentThread().getName() + " terminating");
+    }
+}
 
