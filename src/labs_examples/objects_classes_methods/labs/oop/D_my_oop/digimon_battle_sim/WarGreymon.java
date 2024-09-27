62b9741e927014f0e5e2477ab39df0b932d9e68b
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-27 13:44:33 -0400

    adjusted WarGreymon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/WarGreymon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/WarGreymon.java
@@ -1,4 +1,8 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_sim;
 
-public class WarGreymon {
+public class WarGreymon extends Digimon{
+
+    public WarGreymon() {
+        super();
+    }
 }
