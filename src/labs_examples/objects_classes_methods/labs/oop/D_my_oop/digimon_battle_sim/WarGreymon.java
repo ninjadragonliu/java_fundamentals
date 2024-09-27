8d9ae5d215ffdde89de57fef1174b7bc246921c3
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-27 13:50:42 -0400

    adjusted WarGreymon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/WarGreymon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/WarGreymon.java
@@ -3,6 +3,18 @@ package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_s
 public class WarGreymon extends Digimon{
 
     public WarGreymon() {
-        super();
+        super("WarGreymon", "Vaccine", 200, 65, 45, 35);
+        this.addAttack(new Attack("Nova Blast", 50));
+        this.addAttack(new Attack("Great Horn Attack", 60));
+    }
+
+    @Override
+    public void attack(Digimon target, Attack chosenAttack) {
+
+    }
+
+    @Override
+    public Digimon digivolve() {
+        return null;
     }
 }
