10ac468a3d75aa850a7772d936e88368b86d588f
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-27 13:51:09 -0400

    adjusted MetalGarurumon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/MetalGarurumon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/MetalGarurumon.java
@@ -1,4 +1,10 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_sim;
 
-public class MetalGarurumon {
+public class MetalGarurumon extends Digimon {
+
+    public MetalGarurumon() {
+        super("MetalGarurumon", "Vaccine", 200, 65, 45, 35);
+        this.addAttack(new Attack("Fire", 50));
+        this.addAttack(new Attack("Great Horn Attack", 60));
+    }
 }
