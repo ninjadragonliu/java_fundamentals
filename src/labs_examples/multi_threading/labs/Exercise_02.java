e5e196905ba703b09b7f49d43ce3363392430ea6
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-09-30 12:37:57 -0400

    completed exercise 2

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,26 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+public class Exercise_02 {
+    public static void main(String[] args) {
+        MyThread1 myThread = new MyThread1();
+        myThread.start();
+    }
+}
+
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " starting");
+        try {
+            for (int count = 0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + Thread.currentThread().getName() +
+                        ", count is " + count);
+            }
+        } catch (InterruptedException exc) {
+            System.out.println(Thread.currentThread().getName() + " interrupted");}
+        }
+
+}
