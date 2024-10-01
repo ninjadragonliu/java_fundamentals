bafdee8c3807a4157e3c769e941b420b7f229591
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-01 14:40:57 -0400

    adjusted attack method in WarGreymon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/WarGreymon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/WarGreymon.java
@@ -11,7 +11,7 @@ public class WarGreymon extends Digimon{
 
     @Override
     public void attack(Digimon target, Attack chosenAttack) {
-        System.out.println(this.name + " uses " + chosenAttack.getName() + "!");
+        //System.out.println(this.name + " uses " + chosenAttack.getName() + "!");
         target.defend(chosenAttack.getDamage());
     }
 
