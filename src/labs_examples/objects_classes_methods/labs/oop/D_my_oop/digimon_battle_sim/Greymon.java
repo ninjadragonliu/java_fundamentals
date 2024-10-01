d28bff3f93be27af502b7feb3a2b24cc48dcb52e
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-01 14:43:22 -0400

    adjusted attack method in Greymon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
@@ -9,7 +9,7 @@ public class Greymon extends Digimon{
 
     @Override
     public void attack(Digimon target, Attack chosenAttack) {
-        System.out.println(this.name + " uses " + chosenAttack.getName() + "!");
+        //System.out.println(this.name + " uses " + chosenAttack.getName() + "!");
         target.defend(chosenAttack.getDamage());
     }
 
