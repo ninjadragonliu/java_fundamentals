9a035eb3a445211b2c60bd8fe8facd46614670de
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-02 16:19:15 -0400

    declared Action class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Action.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Action.java
@@ -1,4 +1,14 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_battle_sim;
 
 public class Action {
+    private String name;
+
+    public Action(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;
+    }
 }
