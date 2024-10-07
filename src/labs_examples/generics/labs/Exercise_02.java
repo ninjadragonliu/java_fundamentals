92c2a8bc294db092f6d4459e073ae5b5e48ec211
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-07 12:32:34 -0400

    finished exercise 2

--- src/labs_examples/generics/labs/Exercise_02.java
+++ src/labs_examples/generics/labs/Exercise_02.java
@@ -12,6 +12,12 @@ class Demo{
 
     public static void main(String[] args) {
 
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(Exercise_02.sum(list));
     }
 
 }
