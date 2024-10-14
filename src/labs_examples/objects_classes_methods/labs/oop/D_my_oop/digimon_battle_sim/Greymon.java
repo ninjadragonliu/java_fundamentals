1faf732360f64b2c60aec5ef630f31433390fc16
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-14 15:27:27 -0400

    updated attack method in Greymon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Greymon.java
@@ -10,7 +10,7 @@ public class Greymon extends Digimon{
     @Override
     public void attack(Digimon target, Attack chosenAttack) {
         //System.out.println(this.name + " uses " + chosenAttack.getName() + "!");
-        target.defend(chosenAttack.getDamage());
+        target.takeDamage(chosenAttack.getDamage());
     }
 
     @Override
