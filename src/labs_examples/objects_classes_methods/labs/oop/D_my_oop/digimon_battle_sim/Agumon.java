30c68b0057511124fa9cbd9c88a1f9cddffb6000
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-14 15:19:07 -0400

    updated attack method in Agumon class

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Agumon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_battle_sim/Agumon.java
@@ -12,7 +12,7 @@ public class Agumon extends Digimon{
     @Override
     public void attack(Digimon target, Attack chosenAttack) {
         //System.out.println(this.name + " uses " + chosenAttack.getName() + "!");
-        target.defend(chosenAttack.getDamage());
+        target.takeDamage(chosenAttack.getDamage());
     }
 
     @Override
