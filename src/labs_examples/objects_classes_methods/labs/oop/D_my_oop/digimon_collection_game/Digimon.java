8f7aaab9bd07317e2a96dc68ef1cd678d9d08390
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-21 16:44:44 -0400

    organized code

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_collection_game/Digimon.java
+++
@@ -1,4 +0,0 @@
-package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_collection_game;
-
-public interface Digimon {
-}
---
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/digimon_training_simulator/Digimon.java
@@ -0,0 +1,5 @@
+package labs_examples.objects_classes_methods.labs.oop.D_my_oop.digimon_training_simulator;
+
+public abstract class Digimon {
+
+}
